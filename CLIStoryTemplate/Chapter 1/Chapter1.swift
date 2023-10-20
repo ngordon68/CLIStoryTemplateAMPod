ea49ad25db631e889d9ee3f66f8c28702408f379
ngordon68 <ngordon688@gmail.com>
2023-10-20 13:43:46 -0400

    conditionals

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,6 +5,48 @@
 
 import Foundation
 
+var amountOfPigs:Int = 3
+let pigOneName:String = "Coach"
+let pigTwoName:String = "Mystery"
+let pigThreeName:String = "Johndanial"
+var amountOfHouse:Double = 3.0
+var didWolfBlowHouseDown:Bool = false
+var isHouseStanding:Bool = false
+
 func chapterOne() {
     // Your portion of the story goes here
+    print("One day, there were \(amountOfPigs) pigs and they were all hungry")
+    print("The first pig, \(pigOneName), decided to eat tacos")
+    
+    print("One day there was a wolf that came to the straw house")
+    
+    if isHouseStanding == true {
+        
+        print("The wolf will blow half the house down")
+        amountOfHouse -= 0.5
+        print("Now there \(amountOfHouse) houses left")
+      
+        
+    }
+    else {
+        print("The wolf will eat \(pigOneName)")
+        
+        amountOfPigs -= 1
+        print("Now there are only \(amountOfPigs) left")
+    }
+    
+    print("The wolf then found \(pigTwoName)")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -14,3 +14,8 @@ func readStory() {
 }
 
 readStory()
+
+
+
+
+
